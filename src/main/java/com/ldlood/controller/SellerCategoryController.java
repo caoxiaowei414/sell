a76538992d043e61ae4215e302ad17d048a3914d
Ldlood <xz95@outlokk.com>
2017-08-10 22:26:44 +0800

    后端登陆dao/service

--- src/main/java/com/ldlood/controller/SellerCategoryController.java
+++ src/main/java/com/ldlood/controller/SellerCategoryController.java
@@ -2,15 +2,21 @@ package com.ldlood.controller;
 
 import com.ldlood.dataobject.ProductCategory;
 import com.ldlood.dataobject.ProductInfo;
+import com.ldlood.exception.SellException;
+import com.ldlood.form.CategoryForm;
 import com.ldlood.service.CategoryService;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 
@@ -42,11 +48,38 @@ public class SellerCategoryController {
     public ModelAndView index(@RequestParam(value = "categoryId", required = false) Integer categoryId,
                               Map<String, Object> map) {
 
-        if (categoryId > 0) {
+        if (categoryId != null) {
             ProductCategory productCategory = categoryService.findOne(categoryId);
             map.put("productCategory", productCategory);
         }
 
-        return new ModelAndView("product/index", map);
+        return new ModelAndView("category/index", map);
+    }
+
+    @PostMapping("/save")
+    public ModelAndView save(@Valid CategoryForm form,
+                             BindingResult bindingResult,
+                             Map<String, Object> map) {
+        if (bindingResult.hasErrors()) {
+            map.put("msg", bindingResult.getFieldError().getDefaultMessage());
+            map.put("url", "/sell/seller/category/index");
+            return new ModelAndView("common/error", map);
+        }
+
+        ProductCategory productCategory = new ProductCategory();
+        try {
+            if (form.getCategoryId() != null) {
+                productCategory = categoryService.findOne(form.getCategoryId());
+            }
+            BeanUtils.copyProperties(form, productCategory);
+            categoryService.save(productCategory);
+        } catch (SellException e) {
+            map.put("msg", e.getMessage());
+            map.put("url", "/seller/category/index");
+            return new ModelAndView("common/error", map);
+        }
+
+        map.put("url", "/seller/category/list");
+        return new ModelAndView("common/success", map);
     }
 }
---
+++ src/test/java/com/ldlood/repository/SellerInfoRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.ldlood.repository;
+
+import com.ldlood.dataobject.SellerInfo;
+import com.ldlood.utils.KeyUtil;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ldlood on 2017/8/10.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SellerInfoRepositoryTest {
+    @Autowired
+    private SellerInfoRepository repository;
+
+    @Test
+    public void save() {
+        SellerInfo sellerInfo = new SellerInfo();
+        sellerInfo.setSellerId(KeyUtil.genUniqueKey());
+        sellerInfo.setUsername("admin");
+        sellerInfo.setPassword("admin");
+        sellerInfo.setOpenid("aaaaa");
+
+        SellerInfo result = repository.save(sellerInfo);
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void findByOpenid() throws Exception {
+        SellerInfo result = repository.findByOpenid("aaaaa");
+        Assert.assertEquals("aaaaa", result.getOpenid());
+    }
+
+}
